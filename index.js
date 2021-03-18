ac3d09aa95f5dfee1ee7718aa1bafdd1961c3a1a
Thiago Alberto da Silva <alrrtas@gmail.com>
2021-03-18 10:10:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 var $paragraph = document.querySelector('p')
 
 $paragraph.textContent = 'Conteúdo inserido via javascript'
+
+src="https://unpkg.com/blip-chat-widget" type="text/javascript"
+
+    (function () {
+        window.onload = function () {
+            new BlipChat()
+            .withAppKey('Y2FsbGNlbnRlcmludGVsYnJhczo4NjY2YmVlNS04Y2FkLTQ3NTgtODc4NS1jNDNmZGJjNTQ4NzM=')
+            .withButton({"color":"#00a335","icon":""})
+            .withCustomCommonUrl('https://chat.blip.ai/')
+            .build();
+        }
+    })();
+
+
